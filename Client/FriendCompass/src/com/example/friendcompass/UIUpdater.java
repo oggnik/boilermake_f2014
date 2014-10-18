4f6b815996fc23c78cafe8e0c917076c151bbc7a
crabillm <michael.crabill@gmail.com>
2014-10-18 19:43:34 -0400

    lolz

--- Client/FriendCompass/src/com/example/friendcompass/UIUpdater.java
+++ Client/FriendCompass/src/com/example/friendcompass/UIUpdater.java
@@ -5,6 +5,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView.ScaleType;
 import android.widget.TextView;
 
@@ -23,19 +24,21 @@ public class UIUpdater implements Runnable, SensorEventListener{
 	public void run() {
 		TextView tv = (TextView)cActivity.findViewById(R.id.distanceLabel);
 		tv.setText(message);
-		/*RotateAnimation r = new RotateAnimation((float) cActivity.getAngletonorth(),
+		RotateAnimation r = new RotateAnimation((float) cActivity.getAngletonorth(),
 				(float) (cActivity.getAngletonorth() + 10), 
 				(float) cActivity.getImageView().getWidth()/2, 
 				(float) (cActivity.getImageView().getHeight()/2));
 		cActivity.setAngletonorth((float) cActivity.getAngletonorth() + 10);
-		cActivity.getImageView().startAnimation(r);*/
-		Matrix matrix=new Matrix();
+		r.setRepeatCount(RotateAnimation.INFINITE);
+		cActivity.getImageView().startAnimation(r);
+		
+		/*Matrix matrix=new Matrix();
 		cActivity.getImageView().setScaleType(ScaleType.MATRIX);   //required
 		matrix.postRotate((float) cActivity.getAngletonorth() + 10, 
 				cActivity.getImageView().getDrawable().getBounds().width()/2, 
 				cActivity.getImageView().getDrawable().getBounds().height()/2);
 		cActivity.getImageView().setImageMatrix(matrix);
-		cActivity.setAngletonorth(cActivity.getAngletonorth() + 10);
+		cActivity.setAngletonorth(cActivity.getAngletonorth() + 10);*/
 		
 	}
 	
