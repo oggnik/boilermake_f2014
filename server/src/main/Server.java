40d9a3b773ef0244cd992fa0ea5823d4b4a4fb8f
Nicki Ogg <oggnicki@yahoo.com>
2014-10-18 11:20:10 -0400

    Basic server stuff, accepting connections

--- server/src/main/Server.java
+++ server/src/main/Server.java
@@ -2,16 +2,22 @@ package main;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.ArrayList;
 
 public class Server extends Thread {
 	private ServerSocket serverSocket = null;
-//	private ArrayList<Client> clients;
+	private ArrayList<Client> clients;
+	private boolean listening = false;
 	
 	public Server() {
-		
+		clients = new ArrayList<Client>();
 	}
 	
+	/**
+	 * Accept incoming connections and create a client for each
+	 */
 	public void run() {
+		// Open the server socket
 		try {
 			serverSocket = new ServerSocket(Definitions.SERVER_PORT);
 		} catch (IOException e) {
@@ -19,12 +25,37 @@ public class Server extends Thread {
 			System.out.println(e);
 			System.exit(1);
 		}
+		System.out.println("Server running");
+		
+		// Start listening for connections
+		listening = true;
+		while (listening) {
+			Client client;
+			try {
+				client = new Client(this, serverSocket.accept());
+				client.start();
+			} catch (IOException e) {
+				if (listening) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 	
 	/**
 	 * Stop the server and sever any connections
 	 */
 	public void stopServer() {
-		
+		System.out.println("Server shutting down");
+		listening = false;
+		for (Client c : clients) {
+			c.closeSocket();
+		}
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
