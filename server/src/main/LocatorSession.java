192720fddaccf8671b9f13429f1b9437c7cb61ff
Nicki Ogg <oggnicki@yahoo.com>
2014-10-18 15:32:14 -0400

    Locator print

--- server/src/main/LocatorSession.java
+++ server/src/main/LocatorSession.java
@@ -10,6 +10,7 @@ public class LocatorSession {
 	}
 	
 	public void updateLocation(Client c, double[] location) {
+		System.out.println("Location received from client");
 		// Calculate distance + angle for each client
 		client1.sendMessage("hullo");
 		client2.sendMessage("hmm");
