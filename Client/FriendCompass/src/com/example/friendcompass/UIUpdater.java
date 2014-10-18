c5c2cc915abb7b19af51149ca501e8642375fb78
crabillm <michael.crabill@gmail.com>
2014-10-18 15:23:15 -0400

    a

--- Client/FriendCompass/src/com/example/friendcompass/UIUpdater.java
+++ Client/FriendCompass/src/com/example/friendcompass/UIUpdater.java
@@ -1,18 +1,26 @@
 package com.example.friendcompass;
 
+import android.view.animation.RotateAnimation;
 import android.widget.TextView;
 
 public class UIUpdater implements Runnable {
 	private String message;
 	private CallActivity cActivity;
+	private BufferedImage image;
 	
-	public UIUpdater(CallActivity cAct, String mes) {
+	public UIUpdater(CallActivity cAct, String mes, BufferedImage img) {
 		cActivity = cAct;
 		message = mes;
+		image = img;
 	}
 	
 	public void run() {
 		TextView tv = (TextView)cActivity.findViewById(R.id.distanceLabel);
 		tv.setText(message);
+		RotateAnimation r = new RotateAnimation((float) 1.2, (float) 2.3);
+		image.s
 	}
+	
+	
+	
 }
