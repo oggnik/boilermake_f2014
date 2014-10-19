6a2ce2564fde08772bf76fc06843f727fcacbd48
Nicki Ogg <oggnicki@yahoo.com>
2014-10-18 21:34:01 -0400

    updates for orientation

--- Client/FriendCompass/src/com/example/friendcompass/UIUpdater.java
+++ Client/FriendCompass/src/com/example/friendcompass/UIUpdater.java
@@ -15,18 +15,20 @@ public class UIUpdater implements Runnable{
 		message = mes;
 		String[] parts = mes.split(",");
 		distance = Double.parseDouble(parts[0]);
-		angle = (Double.parseDouble(parts[1]) - cActivity.getRotation() + 360) % 360;
+		double trueAngle = Double.parseDouble(parts[1]);
+		cAct.setTrueAngleToNorth(trueAngle);
+		angle = (trueAngle - cActivity.getRotation() + 360) % 360;
 
 	}
 	
 	public void run() {
 		TextView tv = (TextView)cActivity.findViewById(R.id.distanceLabel);
 		tv.setText(message);
-		RotateAnimation r = new RotateAnimation((float) cActivity.getAngletonorth(),
-				(float) angle, 
+		RotateAnimation r = new RotateAnimation((float) cActivity.getModifiedAngleToNorth(),
+				(float) angle,
 				(float) cActivity.getImageView().getWidth()/2, 
 				(float) (cActivity.getImageView().getHeight()/2));
-		cActivity.setAngletonorth((float) angle);
+		cActivity.setModifiedAngleToNorth((float) angle);
 		r.setRepeatCount(RotateAnimation.INFINITE);
 		cActivity.getImageView().startAnimation(r);
 		
